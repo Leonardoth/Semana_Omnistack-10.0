b95bcd570f29701a3f68e31cc9239f536036946c
LowQualityPlays <lqplays@outlook.com>
2020-01-16 03:35:04 -0300

    Exercicio desafio concluido.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,9 @@
 const {Router} = require('express');
 const DevController = require('./controllers/DevController');
 const SearchController = require('./controllers/SearchController');
+// Desafio
+const UpdateController = require('./controllers/UpdateController');
+const DestroyController = require('./controllers/DestroyController');
 
 const routes = Router();
 
@@ -8,5 +11,7 @@ const routes = Router();
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.get('/search', SearchController.index);
+routes.get('/update', UpdateController.update);
+routes.get('/destroy', DestroyController.destroy);
 
 module.exports = routes;
