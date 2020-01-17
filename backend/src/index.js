6831452e34413c04dd9d4be4855cd601ce8208b1
LowQualityPlays <lqplays@outlook.com>
2020-01-17 00:04:31 -0300

    Backend, added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-7mysn.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 // Métodos HTTP: GET, POST, PUT, DELETE
